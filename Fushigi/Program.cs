089ff78eea84611f773ee2282aa8cab43382dc32
flingus <flingus803@gmail.com>
2025-11-04 05:18:21 +0100

    change version to 1.6.2

--- Fushigi/Program.cs
+++ Fushigi/Program.cs
@@ -11,7 +11,7 @@ using FuzzySharp;
 
 internal class Program
 {
-    public const string Version = "v1.6.1";
+    public const string Version = "v1.6.2";
 
     public static MainWindow MainWindow { get; private set; }
 
